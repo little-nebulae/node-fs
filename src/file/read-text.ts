3b0deedae8d24e3772d37b0c2186782b46627951
Ryan Luong <lumintriet@gmail.com>
2026-09-18 18:20:14 +0700

    feat(file): handle errno exceptions in readTextFile function.

--- src/file/read-text.ts
+++ src/file/read-text.ts
@@ -1,5 +1,18 @@
 import { composeErrorMessage, UnexpectedError } from "@little-nebulae/error";
 import { fail, succeed } from "@little-nebulae/result";
+import {
+  AccessDeniedSystemError,
+  FileTooBigSystemError,
+  IsDirectorySystemError,
+  isErrnoException,
+  NameTooLongSystemError,
+  NoEntrySystemError,
+  NotDirectorySystemError,
+  OutOfMemorySystemError,
+  OverflowSystemError,
+  PermissionDeniedSystemError,
+  SYSTEM_ERROR_RECORD,
+} from "@little-nebulae/system-error";
 import { readFile } from "node:fs/promises";
 
 import { DEFAULT_CHARACTER_ENCODING } from "@/constants";
@@ -9,6 +22,93 @@ export async function readTextFile({ path }: { path: string }) {
     const text = await readFile(path, { encoding: DEFAULT_CHARACTER_ENCODING });
     return succeed(text);
   } catch (error) {
+    if (isErrnoException(error)) {
+      const errno = -(error.errno ?? 0);
+      switch (errno) {
+        case SYSTEM_ERROR_RECORD.PERMISSION_DENIED_SYSTEM_ERROR.errno.number: {
+          return fail(
+            new PermissionDeniedSystemError({
+              message: `The operation to read file at ${path} was prevented by a file seal.`,
+              cause: error,
+              meta: null,
+            }),
+          );
+        }
+        case SYSTEM_ERROR_RECORD.NO_ENTRY_SYSTEM_ERROR.errno.number: {
+          return fail(
+            new NoEntrySystemError({
+              message: `${path} does not exist or is a
+              dangling symbolic link.`,
+              cause: error,
+              meta: null,
+            }),
+          );
+        }
+        case SYSTEM_ERROR_RECORD.OUT_OF_MEMORY_SYSTEM_ERROR.errno.number: {
+          return fail(
+            new OutOfMemorySystemError({
+              message: "Insufficient kernel memory was available.",
+              cause: error,
+              meta: null,
+            }),
+          );
+        }
+        case SYSTEM_ERROR_RECORD.ACCESS_DENIED_SYSTEM_ERROR.errno.number: {
+          return fail(
+            new AccessDeniedSystemError({
+              message: `The requested access to the file is not allowed, or search permission is denied for one of the directories in the path prefix of ${path}`,
+              cause: error,
+              meta: null,
+            }),
+          );
+        }
+        case SYSTEM_ERROR_RECORD.NOT_DIRECTORY_SYSTEM_ERROR.errno.number: {
+          return fail(
+            new NotDirectorySystemError({
+              message: `A component used as a directory in ${path} is not a directory.`,
+              cause: error,
+              meta: null,
+            }),
+          );
+        }
+        case SYSTEM_ERROR_RECORD.IS_DIRECTORY_SYSTEM_ERROR.errno.number: {
+          return fail(
+            new IsDirectorySystemError({
+              message: `${path} is a directory, not a file.`,
+              cause: error,
+              meta: null,
+            }),
+          );
+        }
+        case SYSTEM_ERROR_RECORD.FILE_TOO_BIG_SYSTEM_ERROR.errno.number: {
+          return fail(
+            new FileTooBigSystemError({
+              message: `${path} refers to a file that is too big to be opened.`,
+              cause: error,
+              meta: null,
+            }),
+          );
+        }
+        case SYSTEM_ERROR_RECORD.NAME_TOO_LONG_SYSTEM_ERROR.errno.number: {
+          return fail(
+            new NameTooLongSystemError({
+              message: `${path} or one of its components is too long.`,
+              cause: error,
+              meta: null,
+            }),
+          );
+        }
+        case SYSTEM_ERROR_RECORD.OVERFLOW_SYSTEM_ERROR.errno.number: {
+          return fail(
+            new OverflowSystemError({
+              message: `${path} refers to a regular file that is too large to be opened.`,
+              cause: error,
+              meta: null,
+            }),
+          );
+        }
+      }
+    }
     return fail(
       new UnexpectedError({
         message: composeErrorMessage({
