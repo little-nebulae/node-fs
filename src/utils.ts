d37e301768ca935f0ec7aae63ac6561c9acdcda0
Ryan Luong <lumintriet@gmail.com>
2026-09-18 17:19:12 +0700

    chore: remove files from previous repo.

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { Failure, Success } from "@/types";
-
-export function succeed<TData>(data: TData): Success<TData> {
-  return { success: true, data };
-}
-
-export function fail<TError extends Error>(error: TError): Failure<TError> {
-  return { success: false, error };
-}
