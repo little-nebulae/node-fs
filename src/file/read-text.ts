0c190d6d202504f9b54d3c5edac02dae46d12650
Ryan Luong <lumintriet@gmail.com>
2026-09-18 17:41:58 +0700

    feat(file): add a catch-all error handler to readTextfile function.

--- src/file/read-text.ts
+++ src/file/read-text.ts
@@ -1,8 +1,23 @@
+import { composeErrorMessage, UnexpectedError } from "@little-nebulae/error";
+import { fail, succeed } from "@little-nebulae/result";
 import { readFile } from "node:fs/promises";
 
 import { DEFAULT_CHARACTER_ENCODING } from "@/constants";
 
 export async function readTextFile({ path }: { path: string }) {
-  const text = await readFile(path, { encoding: DEFAULT_CHARACTER_ENCODING });
-  return text;
+  try {
+    const text = await readFile(path, { encoding: DEFAULT_CHARACTER_ENCODING });
+    return succeed(text);
+  } catch (error) {
+    return fail(
+      new UnexpectedError({
+        message: composeErrorMessage({
+          operation: "read text file",
+          reason: "some unexpected error",
+        }),
+        cause: error,
+        meta: null,
+      }),
+    );
+  }
 }
