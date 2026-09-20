d5a7a6ea1107c3e93a1e7eea446273a642003903
Ryan Luong <lumintriet@gmail.com>
2026-09-20 16:39:25 +0700

    test(write-text-file): add 1 more success case.

--- src/file/write-text.test.ts
+++ src/file/write-text.test.ts
@@ -1,5 +1,5 @@
 import { fsTest } from "@test/context";
-import { readFile } from "node:fs/promises";
+import { readFile, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 import { assert, describe, expect } from "vitest";
 
@@ -21,4 +21,24 @@ describe("writeTextFile function should succeed", async () => {
     });
     expect(readText).toBe(text);
   });
+
+  fsTest(
+    "when overwrite data of an existing text file",
+    async ({ tempDirPath }) => {
+      const path = join(tempDirPath, "hello.txt");
+      const oldText = "Hello world.";
+      await writeFile(path, oldText);
+      const newText = "Hello there!";
+
+      const writeResult = await writeTextFile({ path, text: newText });
+      assert(writeResult.success);
+      expect(writeResult.data).toBeUndefined();
+
+      const readText = await readFile(path, {
+        encoding: DEFAULT_CHARACTER_ENCODING,
+      });
+      expect(readText).not.toBe(oldText);
+      expect(readText).toBe(newText);
+    },
+  );
 });
