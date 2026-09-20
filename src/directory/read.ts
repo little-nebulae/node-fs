ac92ca435500a230676f67f2490db4af5462af11
Ryan Luong <lumintriet@gmail.com>
2026-09-20 16:53:09 +0700

    feat(dir/read): defineResult type for readDirectory function.

--- src/directory/read.ts
+++ src/directory/read.ts
@@ -1,3 +1,7 @@
+import type { Result } from "@little-nebulae/result";
+
+import { composeErrorMessage, UnexpectedError } from "@little-nebulae/error";
+import { fail, succeed } from "@little-nebulae/result";
 import { readdir } from "node:fs/promises";
 
 import { DEFAULT_CHARACTER_ENCODING } from "@/constants";
@@ -8,10 +12,23 @@ export async function readDirectory({
 }: {
   path: string;
   recursive?: boolean;
-}) {
-  const entryPaths = await readdir(path, {
-    encoding: DEFAULT_CHARACTER_ENCODING,
-    recursive,
-  });
-  return entryPaths;
+}): Promise<Result<string[], UnexpectedError>> {
+  try {
+    const entryPaths = await readdir(path, {
+      encoding: DEFAULT_CHARACTER_ENCODING,
+      recursive,
+    });
+    return succeed(entryPaths);
+  } catch (error) {
+    return fail(
+      new UnexpectedError({
+        message: composeErrorMessage({
+          operation: "read directory",
+          reason: "some unexpected error",
+        }),
+        cause: error,
+        meta: null,
+      }),
+    );
+  }
 }
