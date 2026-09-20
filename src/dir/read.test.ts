9f8254f13e028d78a022f0576dd5a43192db5aca
Ryan Luong <lumintriet@gmail.com>
2026-09-20 17:26:27 +0700

    test(read-directory): fix import.

--- src/dir/read.test.ts
+++ src/dir/read.test.ts
@@ -3,7 +3,7 @@ import { writeFile } from "node:fs/promises";
 import { join, relative } from "node:path";
 import { assert, describe, expect } from "vitest";
 
-import { readDirectory } from "@/directory/read";
+import { readDirectory } from "@/dir/read";
 
 // Success cases
 describe("readDirectory function should succeed", async () => {
