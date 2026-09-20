6593cf14f75d9058001f51d8237e7fc67b9681f2
Ryan Luong <lumintriet@gmail.com>
2026-09-20 10:02:02 +0700

    test(read-text-file): add 1 more failure case.

--- src/file/read-text.test.ts
+++ src/file/read-text.test.ts
@@ -1,4 +1,4 @@
-import { AbortedError } from "@little-nebulae/error";
+import { AbortedError, isAbortError } from "@little-nebulae/error";
 import {
   isErrnoException,
   SYSTEM_ERROR_RECORD,
@@ -26,6 +26,7 @@ describe("readTextFile function should succeed when", async () => {
   );
 });
 
+// Failure cases
 describe("readTextFile function should fail", async () => {
   fsTest(
     "with an AbortedError instance whose cause isn't an AbortError instance when its operation is aborted and the specified path doesn't exist",
@@ -51,6 +52,30 @@ describe("readTextFile function should fail", async () => {
       expect(caughtError.errno).toBe(
         -SYSTEM_ERROR_RECORD.NO_ENTRY_SYSTEM_ERROR.errno.number,
       );
+    },
+  );
+
+  fsTest(
+    "with an AbortedError instance whose cause is an AbortError instance when its operation is aborted and the specified path does exist",
+    async ({ tempDirPath }) => {
+      const content = "Hello there!";
+      const path = join(tempDirPath, "hello.txt");
+      await writeFile(path, content);
+      const controller = new AbortController();
+      const signal = controller.signal;
+      const abortReason = "Test abort";
+
+      const readPromise = readTextFile({ path, signal });
+      controller.abort(abortReason);
+      const readResult = await readPromise;
+      assert(readResult.success === false);
+      const error = readResult.error;
+      expect(error).toBeInstanceOf(AbortedError);
+      expect(error.meta).toMatchObject({ abortReason });
+
+      const originalError = error.cause;
+      assert(isAbortError(originalError));
+      expect(originalError.cause).toBe(abortReason);
     },
   );
 });
