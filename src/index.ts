9804174281c46e0bcb3efb9b9f32428ce508a8a9
Ryan Luong <lumintriet@gmail.com>
2026-09-20 17:44:49 +0700

    feat: define lib entry.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from "@/constants";
+
+export * from "@/ops/dir/make";
+export * from "@/ops/dir/make-recursively";
+export * from "@/ops/dir/read";
+
+export * from "@/ops/file/read-text";
+export * from "@/ops/file/write-text";
+
+export * from "@/ops/remove-fs-entry";
+export * from "@/ops/resolve-real-path";
