a084639d53219918789bd5851eb8d289d87f5060
Ryan Luong <lumintriet@gmail.com>
2026-09-20 09:47:44 +0700

    test(read-text-file): rewrite the first failure case.

--- src/file/read-text.test.ts
+++ src/file/read-text.test.ts
@@ -1,4 +1,8 @@
 import { AbortedError } from "@little-nebulae/error";
+import {
+  isErrnoException,
+  SYSTEM_ERROR_RECORD,
+} from "@little-nebulae/system-error";
 import { fsTest } from "@test/context";
 import { writeFile } from "node:fs/promises";
 import { join } from "node:path";
@@ -22,21 +26,31 @@ describe("readTextFile function should succeed when", async () => {
   );
 });
 
-describe("readTextFile function should fail when", async () => {
-  fsTest("its promise is aborted", async ({ tempDirPath }) => {
-    // const content = "Hello there!";
-    const path = join(tempDirPath, "hello.txt");
-    // await writeFile(path, content);
-    const controller = new AbortController();
-    const signal = controller.signal;
-    const abortReason = "Test abort";
+describe("readTextFile function should fail", async () => {
+  fsTest(
+    "with an AbortedError instance whose cause isn't an AbortError instance when its operation is aborted and the specified path doesn't exist",
+    async ({ tempDirPath }) => {
+      const path = join(tempDirPath, "hello.txt");
+      const controller = new AbortController();
+      const signal = controller.signal;
+      const abortReason = "Test abort";
 
-    const readPromise = readTextFile({ path, signal });
-    controller.abort(abortReason);
-    const readResult = await readPromise;
-    assert(readResult.success === false);
-    const error = readResult.error;
-    expect(error).toBeInstanceOf(AbortedError);
-    expect(error.cause).toMatchObject({ reason: abortReason });
-  });
+      const readPromise = readTextFile({ path, signal });
+      controller.abort(abortReason);
+      const readResult = await readPromise;
+      assert(readResult.success === false);
+      const error = readResult.error;
+      expect(error).toBeInstanceOf(AbortedError);
+      expect(error.cause).toMatchObject({ reason: abortReason });
+
+      expect(error.meta).toBeDefined();
+      expect(error.meta).toHaveProperty("caughtError");
+      // @ts-expect-error
+      const caughtError = error.meta.caughtError;
+      assert(isErrnoException(caughtError));
+      expect(caughtError.errno).toBe(
+        -SYSTEM_ERROR_RECORD.NO_ENTRY_SYSTEM_ERROR.errno.number,
+      );
+    },
+  );
 });
