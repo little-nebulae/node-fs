ea046c330293ed07b90cf51bf86a824400fff6a9
Ryan Luong <lumintriet@gmail.com>
2026-09-20 17:48:00 +0700

    build(platform): change platform to Node.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   // Build options
   entry: ["./src/index.ts"],
-  platform: "neutral",
+  platform: "node",
   exports: true,
   dts: true,
   // Lint options
